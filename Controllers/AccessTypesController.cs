74b5625dffd5057306694e05704236e1cada693f
Juan Carlos Ramirez <jccortavee@gmail.com>
2021-08-17 16:07:57 -0600

    Add AccessTypesController

---
+++ Controllers/AccessTypesController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AccessControl.Models;
+using AccessControl.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccessControl.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccessTypesController : Controller
+    {
+        private readonly IAccessTypeRepository _accessTypeRepository;
+
+        public AccessTypesController(IAccessTypeRepository accessTypeRepository)
+        {
+            _accessTypeRepository = accessTypeRepository;
+        }
+
+        // GET
+        [HttpGet]
+        public async Task<IReadOnlyList<AccessType>> GetAll()
+        {
+            return await _accessTypeRepository.GetAll();
+        }
+    }
+}
